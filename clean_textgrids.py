5406b7b5c746097ab2580d91b3cee013689b5022
James Tanner <james.tanner@mail.mcgill.ca>
2019-06-18 18:19:37 -0400

    Modify script for SOTC

--- clean_textgrids.py
+++ clean_textgrids.py
@@ -10,49 +10,47 @@ import textgrid
 
 parser = argparse.ArgumentParser()
 parser.add_argument("inputDir", help = "The directory containing textgrids you want to clean")
-parser.add_argument("outputDir", help = "THe directory in which to write the cleaned textgrids")
 args = parser.parse_args()
 
 ## remove punctation from a word if there is any
 def strip_punct(word):
-	return "".join([w for w in word if w not in string.punctuation])
+    return "".join([w for w in word if w not in string.punctuation])
 
-## iterate through subfolders (assuming directory structure
-## same in both input and output folders)
+## iterate through directory
 for root, dirs, files in os.walk(args.inputDir):
-	for directory in dirs:
-		for name in files:
+    for name in files:
+        if name.endswith(".TextGrid"):
+            print("Processing: {}".format(name))
 
-			# find Textgrids
-			if name.endswith(".TextGrid"):
-				print("Processing: {}".format(os.path.join(root, directory, name)))
+            ## read textgrid
+            tg = textgrid.TextGrid()
+            tg.read(os.path.join(root, name))
 
-				tg = textgrid.TextGrid()
-				tg.read(os.path.join(path, file))
+            for tier in tg.getNames():
+                # ignore non-transcript tiers
+                if re.search('(transcript.*)', tier) is None:
+                    continue
+                # make list of 'transcript' tiers
+                transcriptTiers = re.findall('(transcript.*)', tier)
+                for item in transcriptTiers:
+                    pos = tg.getNames().index(item)
 
-				# read transcript tier
-				wt = textgrid.IntervalTier()
+                    # create interval tier and
+                    # and cleaned intervals
+                    wt = textgrid.IntervalTier(name = item)
+                    tr = tg.getList(item)[0]
+                    tg.tiers.pop(pos)
 
-				# Find the name of the transcript tiers
-				# regex match as some tiers have extensions
-				for item in tg.getNames():
-					if re.search('(transcript.*)', item) is None:
-						continue
-					tierName = re.findall('(transcript.*)', item)[0]
-					print("Using {}...".format(tierName))
+                    for interval in tr:
+                        try:
+                            wt.add(interval.minTime, interval.maxTime,
+                                    strip_punct(interval.mark))
+                        except Exception as e:
+                            print(name, e)
 
-					# loop through intervals and remove punctuation
-					for interval in tg.getList(tierName)[0]:
-						wt.add(interval.minTime,interval.maxTime,
-							strip_punct(interval.mark))
-
-					# Remove the original tier and replace
-					# with the punctuation-less version
-					tg.pop(tg.getNames().index(tierName))
-					wt.name = tierName
-
-					# add tier to textgrid and save
-					tg.append(wt)
-					print("Saving as: {}".format(os.path.join(args.outputDir, directory, file)))
-					tg.write(os.path.join(args.outputDir, directory file))
+                    # Remove the original tier and replace
+                    # with the punctuation-less version
+                    tg.append(wt)
+            # save textgrid
+            tg.write(os.path.join(os.path.join(root, name)))
 print("Done!")
