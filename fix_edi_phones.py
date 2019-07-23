a3958e1fab195bfa435e20dbadcf0b54365c2f57
James Tanner <james.tanner@mail.mcgill.ca>
2019-07-23 14:39:19 -0400

    Update Edinburgh script

--- fix_edi_phones.py
+++ fix_edi_phones.py
@@ -13,36 +13,34 @@ args = parser.parse_args()
 
 # phones to check
 phones = {
-	"i" : "i:",
-	"6" : "V",
-	"}:" : "u:",
-	"{I" : "eI",
-	"o" : "@U",
-	"aU@" : "aU"
+    "i" : "i:",
+    "6" : "V",
+    "}:" : "u:",
+    "{I" : "eI",
+    "o" : "@U",
+    "aU@" : "aU"
 }
 
 for root, dirs, files in os.walk(args.inputDir):
-	for name in files:
-		if name.endswith(".TextGrid"):
+    for name in files:
+        if name.endswith(".TextGrid"):
 
-			tg = textgrid.TextGrid()
-			tg.read(os.path.join(root, name), encoding = None)
+            tg = textgrid.TextGrid()
+            tg.read(os.path.join(root, name), encoding = None)
 
-			## iterate through tiers
-			for tier in tg:
-				if tier.name == "MAU":
-					for interval in tier.intervals:
+            ## iterate through tiers
+            for tier in tg:
+                if tier.name == "MAU":
+                    for interval in tier.intervals:
 
+                        # look through phone types on that tier
+                        for k, v in phones.items():
+                            if re.match(k, interval.mark):
+                                print("{}: replacing {} at {} with {}".format(name, interval.mark, interval.minTime, v))
+                                interval.name = re.sub(k, v, interval.mark, 1)
+                                print(interval.mark)
 
-						# look through phone types on that tier
-						for k, v in phones.items():
-							if re.match("^"+ k + "$", interval.mark):
-								# print(interval.mark, interval.minTime, interval.maxTime)
-								print("Replacing {} at {} with {}".format(
-									"^"+ k + "$", interval.minTime, v))
-								interval.name = re.sub("^"+ k + "$", v, interval.mark, 1)
-
-			# overwrite textgrids					
-			print("Saving to {}".format(os.path.join(args.inputDir, name)))
-			tg.write(os.path.join(args.inputDir, name))
+            # overwrite textgrids                   
+            print("Saving to {}".format(os.path.join(args.inputDir, name)))
+            tg.write(os.path.join(args.inputDir, name))
 print("Done!")
