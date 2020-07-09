da92bb8b4fdaedaee52dd0109bd859b4013c67f8
James Tanner <james.tanner@mail.mcgill.ca>
2020-07-09 17:22:56 -0400

    Reformat script

--- glw_dirs.py
+++ glw_dirs.py
@@ -15,41 +15,42 @@ parser.add_argument("inputFiles", help = "Path to the files to be copied")
 parser.add_argument("outputDir", help = "Path to the directory for speaker directories to be made")
 parser.add_argument("--speaker_id", help = "Name of speaker ID column (default = 'speaker_id')", default = "speaker_id")
 parser.add_argument("--file", help = "Name of filename column (default = 'file')", default = "file")
+args = parser.parse_args()
 
 ## read speaker metadata file
 df = pd.read_csv(args.inputCSV)
 
 for index, row in df.iterrows():
 
-	speaker = args.speaker_id
-	file = args.file
+	speaker = row[args.speaker_id]
+	filename = row[args.file]
 
-	print("Processing {} for {}".format(file, speaker))
+	print("Processing {} for {}".format(filename, speaker))
 	## make speaker directory
 	## if it doesn't exist
-	if not os.path.isdir(os.path.join(args.outputDir, row['speaker'])):
-		os.mkdir(os.path.join(args.outputDir, row['speaker']))
+	if not os.path.isdir(os.path.join(args.outputDir, speaker)):
+		print("Making directory for {}".format(speaker))
+		os.mkdir(os.path.join(args.outputDir, speaker))
 
 	## start modifying the textgrid
 	## to just have tiers from
 	## that speaker
-	tg = textgrid.TextGrid()
-	tg_new = textgrid.TextGrid()
+	tg = textgrid.TextGrid(strict = False)
+	tg_new = textgrid.TextGrid(strict = False)
 
-	tg.read(os.path.join(args.inputFiles, row['file'] + ".TextGrid"))
+	tg.read(os.path.join(args.inputFiles, filename + ".TextGrid"))
 
 	for tier in tg:
-
 		## find tiers containing the speaker name
-		if re.match(row['speaker'], tier.name):
+		if re.search(speaker, tier.name):
 			tg_new.append(tier)
 
 	## formulate the output path
-	outPath = os.path.join(args.outputDir, row['speaker'])
+	outPath = os.path.join(args.outputDir, speaker)
 
 	## save textgrid to the new directory
 	## and copy the audio file
-	tg_new.write(os.path.join(outPath, row['file'] + "_" + row['speaker'] + ".TextGrid"))
-	shutil.copy(os.path.join(args.inputCSV, row['file'] + ".wav"), os.path.join(outPath, row['file'] + "_" + row['speaker'] + ".wav"))
+	tg_new.write(os.path.join(outPath, filename + "_" + speaker + ".TextGrid"))
+	shutil.copy(os.path.join(args.inputFiles, filename + ".wav"), os.path.join(outPath, filename + "_" + speaker + ".wav"))
 
 print("Done!")
