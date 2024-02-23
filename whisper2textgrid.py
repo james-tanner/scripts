30efdf689e8b5f63d639f8eee4671f70a795a547
James Tanner <james.robert.tanner@gmail.com>
2024-02-23 10:57:45 +0000

    Manage different text naming conventions

--- whisper2textgrid.py
+++ whisper2textgrid.py
@@ -1,32 +1,52 @@
 import json
+
+from argparse import ArgumentParser
+from pathlib import Path
 from textgrid import TextGrid, IntervalTier, Interval
 
-## read the file
-with open("text.json", "r") as jsonout:
-    out = json.load(jsonout)
+def get_input(fname):
+    with open(fname, "r") as jsonout:
+        out = json.load(jsonout)
+    return out
 
-## create a new empty TextGrid and interval tier
-tg = TextGrid()
-tier = IntervalTier(name = "phones")
+def whisper2textgrid(input_name):
 
-## go through each of the utterance sections
-for utterance in out['segments']:
+    ## read the JSON input file
+    json_input = get_input(input_name)
 
-    ## write each word as a new interval
-    ## based on the whisper output
-    for word in utterance['words']:
-        print(word)
-        interval = Interval(
-            minTime = word['start'],
-            maxTime = word['end'],
-            mark = word['text']
-        )
+    ## create a new empty TextGrid and interval tier
+    tg = TextGrid()
+    tier = IntervalTier(name = "phones")
 
-        ## add that interval to the tier
-        tier.addInterval(interval)
-    print("\n")
+    tname = "text" if "text" in json_input['segments'][0]['words'][0] else "word"
 
-## add the tier to the
-## TextGrid and write out
-tg.append(tier)
-tg.write("text.TextGrid")
+    ## go through each of the utterance sections
+    for utterance in json_input['segments']:
+        ## write each word as a new interval
+        ## based on the whisper output
+        for word in utterance['words']:
+            print(word['start'], word['end'], word[tname])
+            interval = Interval(
+                minTime = word['start'],
+                maxTime = word['end'],
+                mark = word[tname]
+            )
+            tier.addInterval(interval)
+
+    ## add the tier to the
+    ## TextGrid and write out
+    tg.append(tier)
+    print(tg.tiers)
+
+    return tg
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("input", type = Path)
+    parser.add_argument("output", type = Path)
+    args = parser.parse_args()
+
+    input_name = args.input.stem
+    tg_out = whisper2textgrid(args.input)
+
+    tg_out.write(args.output.joinpath(input_name + ".TextGrid"))
