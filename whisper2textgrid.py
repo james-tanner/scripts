296e5502e63e7c96d601b67360e908557d7c89c5
James Tanner <james.robert.tanner@gmail.com>
2024-02-23 11:18:52 +0000

    Handle directory input

--- whisper2textgrid.py
+++ whisper2textgrid.py
@@ -9,8 +9,10 @@ def get_input(fname):
         out = json.load(jsonout)
     return out
 
+
 def get_text_name(input_name):
     return "text" if "text" in input_name['segments'][0]['words'][0] else "word"
+
 
 def make_interval(start, end, text):
     interval = Interval(
@@ -34,6 +36,9 @@ def whisper2textgrid(input_name, verbose):
 
     ## go through each of the utterance sections
     for utterance in inp['segments']:
+        if verbose:
+            print("\n")
+            print(utterance)
         ## write each word as a new interval
         ## based on the whisper output
         for word in utterance['words']:
@@ -41,7 +46,6 @@ def whisper2textgrid(input_name, verbose):
                 print(word['start'], word['end'], word[tname])
 
             interval = make_interval(word['start'], word['end'], word[tname])
-
             tier.addInterval(interval)
 
     ## add the tier to the
@@ -57,7 +61,14 @@ if __name__ == "__main__":
     parser.add_argument("--verbose", "-v", action = "store_true")
     args = parser.parse_args()
 
-    input_name = args.input.stem
-    tg_out = whisper2textgrid(args.input, args.verbose)
+    if args.input.is_file():
+        tg_out = whisper2textgrid(args.input, args.verbose)
+        tg_out.write(args.output.joinpath(args.input.stem + ".TextGrid"))
+    elif args.input.is_dir():
+        inputs = list(args.input.glob("*.json"))
+        for inp in inputs:
+            tg_out = whisper2textgrid(inp, args.verbose)
+            tg_out.write(args.output.joinpath(inp.stem + ".TextGrid"))
+    else:
+        raise("Not a file or directory")
 
-    tg_out.write(args.output.joinpath(input_name + ".TextGrid"))
