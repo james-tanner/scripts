2cefd5595a73498ab1f8dc79368439a56eccdb0a
James Tanner <james.robert.tanner@gmail.com>
2024-02-29 13:28:02 +0000

    Add multispeaker support

--- whisper2textgrid.py
+++ whisper2textgrid.py
@@ -25,39 +25,59 @@ def make_interval(start, end, text):
     return interval
 
 
-def whisper2textgrid(input_name, words = False, verbose = False):
+def get_unique_speakers(transcription):
+    return set([utt['speaker'] for utt in transcription['segments']])
+
+
+def convert_utterance(utterance, tier, text_name, words, verbose):
+
+    if verbose:
+        print("\n")
+        print(utterance)
+
+    if words:
+        for word in utterance['words']:
+            if verbose:
+                print(word['start'], word['end'], word[text_name])
+
+            interval = make_interval(word['start'], word['end'], word[text_name])
+            tier.addInterval(interval)
+
+    else:
+        interval = make_interval(utterance['start'], utterance['end'], utterance['text'])
+        tier.addInterval(interval)
+
+    return tier
+
+
+def whisper2textgrid(input_name, words = False, verbose = False, multispeaker = False):
 
     ## create a new empty TextGrid and interval tier
     tg = TextGrid()
-    tier = IntervalTier(name = "phones")
 
     ## get the name of the text label
     tname = get_text_name(input_name)
 
-    ## go through each of the utterance sections
-    for utterance in input_name['segments']:
-        if verbose:
-            print("\n")
-            print(utterance)
+    if multispeaker:
+        speakers = get_unique_speakers(input_name)
 
-        if words:
-            ## write each word as a new interval
-            ## based on the whisper output
-            for word in utterance['words']:
-                if verbose:
-                    print(word['start'], word['end'], word[tname])
+        for speaker in speakers:
+            if verbose:
+                print(speaker)
+            tier = IntervalTier(name = speaker)
+            for utterance in input_name['segments']:
+                if utterance['speaker'] == speaker:
+                    tier = convert_utterance(utterance, tier, text_name = tname, words = words, verbose = verbose)
+            tg.append(tier)
 
-                interval = make_interval(word['start'], word['end'], word[tname])
-                tier.addInterval(interval)
-        else:
-            interval = make_interval(utterance['start'], utterance['end'], utterance['text'])
-            tier.addInterval(interval)
-
-    ## add the tier to the
-    ## TextGrid and write out
-    tg.append(tier)
+    else:
+        tier = IntervalTier(name = "words")
+        for utterance in input_name['segments']:
+            tier = convert_utterance(utterance, tier, text_name = tname, words = words, verbose = verbose)
+        tg.append(tier)
 
     return tg
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -65,13 +85,14 @@ if __name__ == "__main__":
     parser.add_argument("output", type = Path)
     parser.add_argument("--words", "-w", action = "store_true", help = "Create word-level intervals instead of utterances (default: False)")
     parser.add_argument("--verbose", "-v", action = "store_true", help = "Print transcription information (default: False)")
+    parser.add_argument("--multispeaker", "-ms", action = "store_true", help = "Convert multispeaker transcription (default: False)")
     args = parser.parse_args()
 
     ## check if the input is a single JSON file
     ## or a directory of JSON files
     if args.input.is_file():
         inp_json = get_input(args.input)
-        tg_out = whisper2textgrid(inp_json, args.words, args.verbose)
+        tg_out = whisper2textgrid(inp_json, args.words, args.verbose, args.multispeaker)
         tg_out.write(args.output.joinpath(args.input.stem + ".TextGrid"))
 
     ## if directory, iterate through JSON files
@@ -80,7 +101,7 @@ if __name__ == "__main__":
         for inp in inputs:
             print(f"Converting {inp.stem}")
             inp_json = get_input(inp)
-            tg_out = whisper2textgrid(inp_json, args.words, args.verbose)
+            tg_out = whisper2textgrid(inp_json, args.words, args.verbose, args.multispeaker)
             tg_out.write(args.output.joinpath(inp.stem + ".TextGrid"))
     else:
         raise("Not a file or directory")
