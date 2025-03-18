9f06a53458934cf3291786fe86a66eb573ac7009
james-tanner <james.robert.tanner@gmail.com>
2025-03-18 09:38:03 +0000

    Add TGs

--- make_vcs_corpus.py
+++ make_vcs_corpus.py
@@ -5,10 +5,15 @@ from argparse import ArgumentParser
 from pathlib import Path
 from tqdm import tqdm
 
+def get_files(regex, wav, tg_dir):
+    speaker_name = regex.search(wav.stem).groups()[0]
+    tg = tg_dir.joinpath(wav.stem).with_suffix(".TextGrid")
+    return (speaker_name, wav, tg)
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("wav_dir", type = Path)
-    # parser.add_argument("tg_dir", type = Path)
+    parser.add_argument("tg_dir", type = Path)
     parser.add_argument("out_dir", type = Path)
     parser.add_argument("--clean", action = "store_true")
     args = parser.parse_args()
@@ -23,15 +28,19 @@ if __name__ == "__main__":
 
     ## determine the mapping between speakers and files
     speaker_regex = re.compile(r"Vcs_(.*)\_naming")
-    speakers_wavs_tups = [(speaker_regex.search(wav.stem).groups()[0], wav) for wav in wavs]
-    speakers = set([speaker for speaker, _ in speakers_wavs_tups])
+    speaker_wavs_tgs = [get_files(speaker_regex, wav, args.tg_dir) for wav in wavs]
+    speakers = set([tup[0] for tup in speaker_wavs_tgs])
 
     for speaker in tqdm(speakers):
         speaker_dir = args.out_dir.joinpath(speaker)
         speaker_dir.mkdir(parents = True, exist_ok = True)
 
-        for spk, wav in speakers_wavs_tups:
+        for spk, wav, tg in speaker_wavs_tgs:
             if spk == speaker:
                 shutil.copy(wav, speaker_dir)
+                try:
+                    shutil.copy(tg, speaker_dir)
+                except FileNotFoundError:
+                    pass
 
     print("Done!")
